e929347138936230b76a9403e3834dd9714d768e
HoopShop.TH <119285445+Phiphitthon@users.noreply.github.com>
2024-11-26 11:27:44 +0700

    add

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "nattapon Saengkhiao";
+string student_id = "673450034-8";
+// string is Drtrtype -> ชนิดข้อมูล
+Console.WriteLine("Hello, World!");
+Console.WriteLine("my name is nattapon Saengkhiao");
+Console.WriteLine("my student id: 673450034-8");
